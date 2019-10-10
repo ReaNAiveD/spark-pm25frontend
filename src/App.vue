df97dc26d1d62d573a070a39da1fd4bb8913ab4c
NAiveD <32201005+ReaNAiveD@users.noreply.github.com>
2019-10-11 03:25:49 +0800

    base chart

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <ve-line :data="pmData" :setting="this.chartSettings"></ve-line>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        data: function (){
+            this.chartSettings = {
+                xAxisType : 'time',
+                labelMap: {
+                    'time': '时间',
+                    'other': '其他'
+                },
+                scale : [true, true]
+            };
+            return {
+                socketHost: "localhost",
+                socketPort: "8080",
+                pmData: {
+                    columns: ['time', 'PM2.5', 'other'],
+                    rows: []
+                }
+            }
+        },
+        created(){
+            this.initWebSocket();
+        },
+        methods: {
+            initWebSocket(){
+                this.websocket = new WebSocket(this.socketHost + ":" + this.socketPort + "/pm/websocket");
+                this.websocket.onmessage = function (message) {
+                    this.pmData.rows.push(message);
+                }
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
+import VeLine from 'v-charts/lib/line.common'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.component(VeLine.name, VeLine);
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
